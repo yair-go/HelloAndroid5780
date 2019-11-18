0e1bbd75d603ac5f6f29f2e45e7501487148466b
Yair Goldshtein <irigo1@gmail.com>
2019-11-18 19:54:26 +0200

    intent

--- app/src/main/java/com/yair/helloandroid/MainActivity.java
+++ app/src/main/java/com/yair/helloandroid/MainActivity.java
@@ -1,5 +1,6 @@
 package com.yair.helloandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,11 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
         Button HiButton = (Button)findViewById(R.id.hi_button);
 
+        Button sleep_button = (Button)findViewById(R.id.sleep_button);
 
         HiButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, R.string.hi_message, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        sleep_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                startActivity(intent);
             }
         });
     }
